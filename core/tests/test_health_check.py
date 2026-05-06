d8fed33e41ae9b980bb74a521af8652c3d87a202
Lucassribeiro9 <lucas.devlop94@gmail.com>
2026-05-06 17:02:49 -0300

    feat(core): criado o app core e o health check

---
+++ core/tests/test_health_check.py
@@ -0,0 +1,13 @@
+""" Health check tests """
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+class HealthCheckTests(SimpleTestCase):
+    """ Tests de health check endpoint """
+
+    def test_health_check_return_ok(self):
+        """ Testa se o health check retorna o status code 200 """
+        
+        response = self.client.get(reverse('core:health-check'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'OK')
