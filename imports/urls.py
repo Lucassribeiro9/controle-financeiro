f55271d7fbe1539721e17d0845eed81f120c303e
Lucas Ribeiro <57766036+Lucassribeiro9@users.noreply.github.com>
2026-05-17 19:06:00 -0300

    feat: adiciona templates de imports

--- imports/urls.py
+++ imports/urls.py
@@ -8,7 +8,9 @@ app_name = "imports"
 
 urlpatterns = [
     path("imports/upload/", views.upload_import, name="upload"),
+    path("imports/upload/page/", views.upload_import_page, name="upload-page"),
     path("imports/review/", views.review_imports, name="review"),
+    path("imports/review/page/", views.review_imports_page, name="review-page"),
     path(
         "imports/<int:imported_transaction_id>/confirm/",
         views.confirm_import,
